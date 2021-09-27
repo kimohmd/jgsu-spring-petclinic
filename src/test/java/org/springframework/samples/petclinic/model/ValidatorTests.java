c21d969744660ed0475d2d5d7ea4736403ad3167
kimo <abdel-karim.hammad@univ-rouen.fr>
2021-09-27 14:44:48 +0200

    break a test

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("");
+		person.setFirstName("hgkfkhygf");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
